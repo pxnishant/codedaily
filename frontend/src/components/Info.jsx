c9f55d358cc267acb11827d2c6dd988341d6586d
Nishant Singh <nishantsvis@gmail.com>
2024-09-24 23:59:05 +0530

    added information component

---
+++ frontend/src/components/Info.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './info.css'
+
+export default function Info() {
+  return (
+    <>
+        <div className='info-box'>
+                <h3>Get Leetcode questions on your email, daily.</h3>
+        </div>
+    </>
+  )
+}
