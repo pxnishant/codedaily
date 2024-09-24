6d4dd85d0556f124a6760858b1d039d99960f33b
Nishant Singh <nishantsvis@gmail.com>
2024-09-25 04:41:59 +0530

    changed readme location, minor updates

--- frontend/README.md
+++
@@ -1,8 +0,0 @@
-# CodeDaily
-
-This is a project that I'm very enthusiastic about.
-
-## Goal
-To make a website that emails you one or more LeetCode questions daily, based on the difficulty, topics, and number of questions that you have chosen.
-## Status
-Currently building.
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,22 @@
-import Login from "./components/Login"
-import Info from "./components/Info"
+import Header from './components/Header.jsx'
+import Faq from './components/Faq.jsx'
+import Login from './components/Login.jsx'
 
 function App() {
-
   return (
     <main>
-
-    <div className="information">
-          <Info/>
-    </div>
-    <div className="login">
-        <Login/>
-    </div>
-
-
-    <div className="information">
-          <Info/>
-    </div>
-
+      <Header/>
+      <div className="main-content">
+        <div className='faq-section'>
+          <Faq title = "Why does this website do?" content = "This website emails you one or more LeetCode questions daily, depending on the difficulty and topics that you select."/>
+          <Faq title = "How are these questions picked?" content = "Questions have been picked among the 500 most-liked questions on LeetCode."/>
+          <Faq title = "Is it free?" content = "It's absolutely free. I'm also working on integrating Codeforces, it will be available soon."/>
+        </div>
+          <Login/>
+      </div>
     </main>
-  )
+
+  );
 }
 
-export default App
+export default App;
