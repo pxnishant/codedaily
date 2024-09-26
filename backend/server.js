8059d171dc66bec50093354902d7335c3b3160a2
Nishant Singh <nishantsvis@gmail.com>
2024-09-26 20:10:35 +0530

    connected backend + frontend + mongodb

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,13 @@ import passport from 'passport';
 import cors from 'cors'
 import authRoute from "./routes/auth.js"
 dotenv.config();
+
 import passportStrategy from "./passport.js"
+
+import mongoose from 'mongoose'
+import User from './database/User.js'
+
+mongoose.connect(process.env.MONGODB_URI)
 
 const app = express()
 const PORT = process.env.PORT || 8080;
@@ -33,5 +39,46 @@ app.use(cors({
 }));
 
 app.use("/auth", authRoute)
+
+app.post('/updateuser', async (req, res) => {
+
+    try {
+
+        const { email, difficulty, topics } = req.body;
+
+        const count = await User.countDocuments({ email: email });
+
+        if (count === 0) {
+
+            await User.create({
+                email: email,
+                difficulty: difficulty,
+                topics: topics
+            });
+
+            res.status(201).json({ message: 'User successfully added' });
+
+        }
+        
+        else {
+
+            await User.updateOne({ email: email }, {
+                difficulty: difficulty,
+                topics: topics
+            });
+
+            res.status(200).json({ message: 'User successfully updated' });
+
+        }
+
+    }
+    
+    catch (error) {
+
+        res.status(500).json({ message: 'Error adding/updating user', error: error.message });
+        
+    }
+});
+
 
 app.listen(PORT, () => console.log(`Server running, port: ${PORT}`))
