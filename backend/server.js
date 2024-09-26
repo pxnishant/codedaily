6c59e8f6aee93c681e16aad284aecf2f16bee97a
Nishant Singh <nishantsvis@gmail.com>
2024-09-27 03:20:53 +0530

    trying to make auth work

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ mongoose.connect(process.env.MONGODB_URI)
 const app = express()
 const PORT = process.env.PORT || 8080;
 app.use(express.json());
-
+app.set('trust proxy', 1)
 app.use(
     cookieSession(
         {
