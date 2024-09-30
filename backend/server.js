caa54d2557eaa7afb40fbf12dab7603a6f0a2260
Nishant Singh <nishantsvis@gmail.com>
2024-09-30 13:44:02 +0530

    added welcome email on signups, improved APIs

--- backend/server.js
+++ backend/server.js
@@ -47,7 +47,7 @@ app.use(session({
   cookie: {
     maxAge: 365 * 24 * 60 * 60 * 1000,
     secure: (process.env.NODE_ENV === 'production'),
-    sameSite: "none", 
+    sameSite: process.env.NODE_ENV === 'production' ? "none" : "lax", 
     httpOnly: (process.env.NODE_ENV == 'production')
     }
 }))
