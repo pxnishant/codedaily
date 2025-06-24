ec1bbc1cdb5302ba640a90e5ab96b520de858a19
Nishant Singh <nishantsvis@gmail.com>
2025-06-24 13:02:50 +0530

    Integrated new APIs

--- backend/routers/mainRoutes.js
+++ backend/routers/mainRoutes.js
@@ -1,13 +1,13 @@
 const getData = require('../controllers/getData')
-const updateUser = require('../controllers/updateUser')
-const sendAll = require('../controllers/sendAll.js')
+const updateProblems = require('../controllers/updateProblems')
+// const sendAll = require('../controllers/sendAll.js')
 const authMW = require('../middlewares/authMW.js')
 const express = require('express')
 const router = express.Router()
 
-router.get('/getdata', authMW, getData)
-router.post('/updateuser', authMW, updateUser)
-router.get('/sendall', sendAll)
+router.get('/getData', authMW, getData)
+router.post('/updateProblems', authMW, updateProblems)
+// router.get('/sendall', sendAll)
 router.get('/', async (req, res) => {
     res.send('CodeDaily server working perfectly!')
 })
--- backend/server.js
+++ backend/server.js
@@ -24,11 +24,10 @@ connectDB()
 
 const PORT = process.env.PORT || 8080;
 
-
 app.use(cors({
-    origin: process.env.CLIENT_URL,  
+    origin: `http://${process.env.CLIENT_URL}`,  
     methods: "GET,POST,PUT,PATCH,DELETE,OPTIONS",
-    allowedHeaders: ['Content-Type', 'Authorization', 'Accept', 'X-Correlation-ID', 'email'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'email'],
     credentials: true  
 }));
 
