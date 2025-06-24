fddbfcc726e30c039946d9cb6eb6bc2674fcf389
Nishant Singh <nishantsvis@gmail.com>
2025-06-24 16:22:16 +0530

    Implemented daily-emailing service

--- backend/routers/mainRoutes.js
+++ backend/routers/mainRoutes.js
@@ -1,11 +1,12 @@
 const getData = require('../controllers/getData')
 const updateProblems = require('../controllers/updateProblems')
-// const sendAll = require('../controllers/sendAll.js')
+const sendAll = require('../controllers/sendAll.js')
 const authMW = require('../middlewares/authMW.js')
 const express = require('express')
 const router = express.Router()
 
 router.get('/getData', authMW, getData)
+router.get('/sendAll', sendAll)
 router.post('/updateProblems', authMW, updateProblems)
 // router.get('/sendall', sendAll)
 router.get('/', async (req, res) => {
