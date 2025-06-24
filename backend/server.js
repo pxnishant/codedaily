ac8976c8ba577371f9d53aba955c29ad95254985
Nishant Singh <nishantsvis@gmail.com>
2025-06-24 13:31:07 +0530

    Trying to fix login

--- backend/server.js
+++ backend/server.js
@@ -25,15 +25,13 @@ connectDB()
 const PORT = process.env.PORT || 8080;
 
 app.use(cors({
-    origin: `${process.env.CLIENT_URL}`,  
+    origin: process.env.CLIENT_URL,  
     methods: "GET,POST,PUT,PATCH,DELETE,OPTIONS",
-    allowedHeaders: ['Content-Type', 'Authorization', 'email'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Accept', 'X-Correlation-ID', 'email'],
     credentials: true  
 }));
-
+app.options('*', cors());
 app.use(express.json());
-app.use(cookieParser())
-
 app.use('/auth', authRoutes)
 app.use('/', mainRoutes)  
 
